a45659b83cc6af132b794756fe3a7c6e1f882602
tropicalhobo <gabtorres1011@gmail.com>
2017-01-17 14:15:45 +0800

    Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   mosaic regression/batch_clip.py

---
+++ mosaic regression/batch_clip.py
@@ -0,0 +1,34 @@
+import os
+import glob
+from subprocess import call
+
+
+def clip_raster(fn, shp, outdir):
+
+    path, name = os.path.split(fn)
+    print '\nclipping ' + name + ' to ' + outdir
+    clipped = outdir+'\clip_' + name.split('.')[0] + '.TIF'
+    clip_cmd = ['gdalwarp', '-srcnodata', '-99', '-cutline', shp,
+                '-crop_to_cutline', fn, clipped]
+    call(clip_cmd)
+
+    return
+
+def main():
+    work_dir = "G:\LUIGI\ICLEI\IMAGE PROCESSING\IMAGES\LC81140512014344LGN00"
+    ras_list = glob.glob(work_dir+'\*.TIFF')
+    clip_shp = "D:\LUIGI\ICLEI\IMAGE PROCESSING\IMAGES\\brgyb_1999_buffer.shp"
+
+    for root, dirs, files in os.walk(work_dir):
+        #print root
+        for f in files:
+            #print f
+            if '.xml' in f:
+                pass
+            elif '.TIF' in f:
+                print f
+                file_path = root+'\\'+f
+                clip_raster(file_path, clip_shp, root)
+
+if __name__ == "__main__":
+    main()
