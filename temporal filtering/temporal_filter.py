28fb4386189368dd90d1d112a264609073019fea
tropicalhobo <gabtorres1011@gmail.com>
2017-02-03 16:59:36 +0800

    partial pseudo-code for temporal filtering

--- temporal filtering/temporal_filter.py
+++ temporal filtering/temporal_filter.py
@@ -4,6 +4,7 @@
 #perform linear regression
 
 import gdal
+import sys
 import time as tm
 import numpy as np
 import scipy
@@ -16,12 +17,35 @@ from matplotlib import pyplot as plt
 from matplotlib import colors
 
 
+def open_image(directory):
+    image_ds = gdal.Open(directory, GA_ReadOnly)
+
+    if image_ds is None:
+        print 'Could not open ' + directory
+        sys.exit(1)
+
+    return image_ds
+
+
+def get_img_param(image_dataset):
+    cols = image_dataset.RasterXSize
+    rows = image_dataset.RasterYSize
+    num_bands = image_dataset.RasterCount
+    img_gt = image_dataset.GetGeoTransform()
+    img_proj = image_dataset.GetProjection()
+    img_driver = image_dataset.GetDriver()
+
+    img_params = [cols, rows, num_bands, img_gt, img_proj, img_driver]
+
+    return img_params
+
+
 def compute_ndvi(band_array):
     pass
 
 
 def downscale_image():
-    # compute downscale factor
+    # compute downscale factor by using image parameters of landsat and wv2
     pass
 
 
@@ -33,6 +57,8 @@ def main():
     # Open Landsat
     landsat_dir = r""
     wv2_dir = r""
+
+    # retrieve image parameters
 
     # compute landsat ndvi
 
