ffb56207450098848a84746fe4cfab16f33fb813
tropicalhobo <gabtorres1011@gmail.com>
2017-02-07 17:11:10 +0800

    remove compute ndvi function, as there is a separate script for that

--- temporal filtering/temporal_filter.py
+++ temporal filtering/temporal_filter.py
@@ -40,8 +40,41 @@ def get_img_param(image_dataset):
     return img_params
 
 
-def compute_ndvi(band_array):
-    pass
+def output_ds(out_array, img_params, fn='result.tif'):
+    # create output raster data-set
+    cols = img_params[0]
+    rows = img_params[1]
+    bands = 1  # ndvi image needs only one band
+    gt = img_params[3]
+    proj = img_params[4]
+    driver = gdal.GetDriverByName('GTiff')
+    driver.Register()
+
+    out_ras = driver.Create(fn, cols, rows, bands, GDT_Float32)
+    out_ras.SetGeoTransform(gt)
+    out_ras.SetProjection(proj)
+
+    out_band = out_ras.GetRasterBand(1)
+
+    out_band.WriteArray(out_array)
+
+    out_band.SetNoDataValue(0)
+    out_band.FlushCache()
+    out_band.GetStatistics(0, 1)
+
+    return
+
+
+def compute_ndvi(image):
+    ir_band = image.GetRasterBand(4).ReadAsArray(0, 0).astype(np.float16)
+    nir_band = image.GetRasterBand(5).ReadAsArray(0, 0).astype(np.float16)
+
+    mask = np.greater(ir_band + nir_band, 0)
+    ndvi = np.choose(mask, (0, (nir_band - ir_band) / (nir_band + ir_band)))
+    #ndvi = (nir_band - ir_band) / (nir_band + ir_band + 0.00000000001)
+    #ndvi = nir_band - ir_band
+
+    return ndvi
 
 
 def downscale_image():
@@ -54,14 +87,21 @@ def temporal_mask():
 
 
 def main():
-    # Open Landsat
-    landsat_dir = r""
+    # Open Landsat and WV2 ndvi images
+    #landsat_dir = r"subject image/sub.vrt"
+    landsat_dir = r"regression_results.tif"
     wv2_dir = r""
 
+    landsat_img = open_image(landsat_dir)
+
     # retrieve image parameters
+    landsat_param = get_img_param(landsat_img)
+    #print landsat_param
 
     # compute landsat ndvi
+    ndvi = compute_ndvi(landsat_img)
 
+    output_ds(ndvi, landsat_param, fn='ndvi.tif')
 
 if __name__ == "__main__":
     start = tm.time()
